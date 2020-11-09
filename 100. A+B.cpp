395fc7b456a42399c4aa57c737241635a532ed0b
Akib Bishwas <39456189+AkibBishwas@users.noreply.github.com>
2020-11-10 01:41:50 +0600

    Add files via upload

---
+++ 100. A+B.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+   long long  int a,d,n;
+
+   cin>>d>>a;
+   n=a+d;
+   cout<<n<<endl;
+
+ return 0;
+}
