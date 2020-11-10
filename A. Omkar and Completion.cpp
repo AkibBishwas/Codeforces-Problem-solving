8bf4d81fdf89c5ab678a30bf4342629a5545ccc5
Akib Bishwas <39456189+AkibBishwas@users.noreply.github.com>
2020-11-10 09:34:11 +0600

    Add files via upload

---
+++ A. Omkar and Completion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    long long int n,l,k,j,i,p,m,a,x,c,b;
+    cin>>n;
+    l=999;
+    for(i=0;i<n;i++)
+        cout<<l<<" ";
+        cout<<endl;
+    }
+        return 0;
+}
