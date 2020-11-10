6e75efabf6f326ba86f1b1f86f9640c5629e7672
Akib Bishwas <39456189+AkibBishwas@users.noreply.github.com>
2020-11-10 09:44:29 +0600

    Add files via upload

---
+++ A. Maximum GCD.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+   cin>>x;
+    while(x--){
+    long long int t,m,n,i,j,l,k,a,b;
+    cin>>n;
+    if(n%2==0)
+        cout<<n/2<<endl;
+    else
+    {
+        l=n-1;
+        a=l/2;
+        cout<<a<<endl;
+    }
+    }
+return 0;
+  }
