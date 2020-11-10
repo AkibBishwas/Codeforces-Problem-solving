da52a38eb2ffd685e4bc26656daac98c49a1ae85
Akib Bishwas <39456189+AkibBishwas@users.noreply.github.com>
2020-11-10 09:59:06 +0600

    Add files via upload

---
+++ C. K-th Not Divisible by n.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+     cin>>t;
+     while(t--)
+     {
+    long long int n,k,i,l,j,h,ck;
+    cin>>n>>k;
+   h=(k-1)/(n-1);
+   l=h+k;
+   cout<<l<<endl;
+     }
+    return 0;
+}
